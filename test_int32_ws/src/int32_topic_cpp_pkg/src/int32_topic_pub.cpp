513a491c0a48c9ee4168f188c5ac8faf7b1f9b76
Oliver Cao <doracaowei0511@gmail.com>
2025-08-29 20:40:41 +0200

    include path

--- test_int32_ws/src/int32_topic_cpp_pkg/src/int32_topic_pub.cpp
+++ test_int32_ws/src/int32_topic_cpp_pkg/src/int32_topic_pub.cpp
@@ -1,3 +1,11 @@
 #include <functional>
 #include <string>
 #include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class Test_Publisher : public rclcpp::Node
+{
+    
+}
